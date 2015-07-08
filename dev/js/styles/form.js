0cf0cb596a73f52b8ed45079f074a8c5c90b3727
kudohamu <kudohamu91086@yahoo.co.jp>
2015-07-08 21:16:20 +0900

    フォームの背景色を変更

--- dev/js/styles/form.js
+++ dev/js/styles/form.js
@@ -1,6 +1,7 @@
 export default {
   container: {
-    backgroundColor:"rgba(100,100,255,0.1)",
+    //backgroundColor:"rgba(100,100,255,0.1)",
+    backgroundColor:"rgba(255,255,255,0.6)",
     borderRadius:"6px",
     padding:"25px",
   }
