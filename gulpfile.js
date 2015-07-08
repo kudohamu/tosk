b58bdd58c8cf1589606def70463dec86c40db20f
kudohamu <kudohamu91086@yahoo.co.jp>
2015-07-08 12:21:41 +0900

    gulpfileからcssの設定を削除

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,31 @@
 var gulp = require('gulp');
-var connect = require('gulp-connect');
 var browserify = require('browserify');
+var babelify = require("babelify");
 var source = require('vinyl-source-stream');
 var reactify = require('reactify');
 var sync = require('browser-sync').create();
-var plumber = require('gulp-plumber');
-var concat = require('gulp-concat');
 
 var dev_dir = 'dev';
 var prod_dir = 'prod';
+var scss_path = dev_dir + '/sass/**/*.scss';
 var js_path = dev_dir + '/js/**/**/*.js';
 var jsx_path = dev_dir + '/js/views/**/*.jsx';
 
-gulp.task('browserify', function(){
+gulp.task('js_compile', function(){
   var b = browserify({
     entries: [dev_dir + '/js/views/main.js'],
-    transform: [reactify],
     extensions: ['.js', '.jsx']
   });
-  return b.bundle()
-    .pipe(plumber())
+  return b.transform(babelify)
+    .bundle()
+    .on("error", function (err) { console.log("Error : " + err.message); })
     .pipe(source('app.js'))
     .pipe(gulp.dest(prod_dir + '/js'))
     .pipe(sync.stream());
 });
 
 gulp.task('js_watch', function(){
-  gulp.watch([jsx_path, js_path], ['browserify']).on('change', sync.reload);
+  gulp.watch([jsx_path, js_path], ['js_compile']).on('change', sync.reload);
 });
 
 gulp.task('browser-sync', function() {
@@ -37,4 +36,4 @@ gulp.task('browser-sync', function() {
   });
 });
 
-gulp.task('default', ['browserify', 'js_watch', 'browser-sync']);
+gulp.task('default', ['js_compile', 'js_watch', 'browser-sync']);
