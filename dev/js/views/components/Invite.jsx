44c8e8851630e50d60d4a24d5e6aac0d2a86dfed
kudohamu <kudohamu91086@yahoo.co.jp>
2015-07-16 17:00:13 +0900

    create: Logs Component

---
+++ dev/js/views/components/Invite.jsx
@@ -0,0 +1,24 @@
+import React from 'react/addons';
+import Radium from 'radium';
+import Vendor from 'react-vendor-prefix';
+
+var styles = Vendor.prefix({
+});
+
+class Invite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+Invite.propTypes = {
+};
+
+export default Radium(Invite);
---
+++ dev/js/views/components/Logs.jsx
@@ -0,0 +1,24 @@
+import React from 'react/addons';
+import Radium from 'radium';
+import Vendor from 'react-vendor-prefix';
+
+var styles = Vendor.prefix({
+});
+
+class Logs extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+Logs.propTypes = {
+};
+
+export default Radium(Logs);
