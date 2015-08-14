4ae0633bcfd687428796b9281f560e0ae2b497b0
kudohamu <kudohamu91086@yahoo.co.jp>
2015-08-15 00:02:24 +0900

    created: TODOAPIUtils

---
+++ dev/js/utils/TODOAPIUtils.js
@@ -0,0 +1,26 @@
+import request from 'superagent';
+import Constants from '../constants/Constants';
+import AppDispatcher from '../dispatcher/Dispatcher';
+import APIUtils from './APIUtils';
+
+import TODOStore from '../stores/TODOStore';
+import ChannelStore from '../stores/ChannelStore';
+
+export default {
+  join: (boardId) => {
+    ChannelStore.setChan(`todos:${boardId}`);
+
+    ChannelStore.getChan().join().receive("ok", chan => {
+    });
+  },
+
+  on: (message, callback) => {
+    ChannelStore.getChan().on(message, (payload) => {
+      callback(payload);
+    });
+  },
+
+  push: (message, payload) => {
+    ChannelStore.getChan().push(message, payload);
+  }
+}
