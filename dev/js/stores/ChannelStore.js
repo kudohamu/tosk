73a141e0a96ee43d6bab7f76f1b28678a4dfd857
kudohamu <kudohamu91086@yahoo.co.jp>
2015-08-15 00:07:17 +0900

    created: ChannelStore

---
+++ dev/js/stores/ChannelStore.js
@@ -0,0 +1,52 @@
+import AppDispatcher from '../dispatcher/Dispatcher';
+import Constants from '../constants/Constants';
+import { EventEmitter } from 'events';
+
+import {Socket} from 'libphoenix';
+
+const ActionTypes = Constants.ActionTypes;
+const CHANGE_EVENT = 'change';
+
+var _socket = {};
+var _chan = {};
+
+class ChannelStore extends EventEmitter {
+  emitChange() {
+    this.emit(CHANGE_EVENT);
+  }
+  
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
+  }
+  
+  removeChangeListener(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+
+  connect() {
+    _socket = new Socket('ws://localhost:4000/ws/');
+    _socket.connect();
+  }
+
+  setChan(channel) {
+    _chan = _socket.chan(channel, {});
+  }
+
+  getChan() {
+    return _chan;
+  }
+}
+
+let _ChannelStore = new ChannelStore();
+
+export default _ChannelStore;
+
+AppDispatcher.register((payload) => {
+  const action = payload.action;
+
+  switch(action.type) {
+    case ActionTypes.Hoge:
+      _ChannelStore.emitChange();
+      break;
+  }
+});
