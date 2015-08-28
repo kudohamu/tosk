66fb4277038d727b6358fd56de00bc3de529cdd6
kudohamu <kudohamu91086@yahoo.co.jp>
2015-08-28 11:14:03 +0900

    added: CHANNEL_NAME

--- dev/js/utils/TODOAPIUtils.js
+++ dev/js/utils/TODOAPIUtils.js
@@ -6,34 +6,36 @@ import APIUtils from './APIUtils';
 import TODOStore from '../stores/TODOStore';
 import ChannelStore from '../stores/ChannelStore';
 
+const CHANNEL_NAME = 'todo';
+
 export default {
   join: (boardId) => {
     const topic = `todos:${boardId}`;
-    ChannelStore.setChan(topic);
-    ChannelStore.registerTopic(topic);
+    ChannelStore.setChan(CHANNEL_NAME, topic);
+    ChannelStore.registerTopic(CHANNEL_NAME, topic);
 
-    ChannelStore.getChan().join().receive("ok", chan => {
+    ChannelStore.getChan(CHANNEL_NAME).join().receive("ok", chan => {
     });
   },
 
   leave: () => {
-    ChannelStore.getChan().leave().receive("ok", chan => {
+    ChannelStore.getChan(CHANNEL_NAME).leave().receive("ok", chan => {
     });
   },
 
   on: (message, callback) => {
-    ChannelStore.getChan().on(message, (payload) => {
+    ChannelStore.getChan(CHANNEL_NAME).on(message, (payload) => {
       callback(payload);
     });
   },
 
   off: (message) => {
-    ChannelStore.getChan().off(message);
+    ChannelStore.getChan(CHANNEL_NAME).off(message);
   },
 
   push: (message, payload) => {
     setTimeout(() => {
-      ChannelStore.getChan().push(message, payload);
+      ChannelStore.getChan(CHANNEL_NAME).push(message, payload);
     }, 100);
   }
 }
