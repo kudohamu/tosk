65fadf2a98b8a59224c7ed499937b3f30caa6a3a
kudohamu <kudohamu91086@yahoo.co.jp>
2015-08-30 08:10:06 +0900

    added: selecting board event listener

--- dev/js/stores/BoardStore.js
+++ dev/js/stores/BoardStore.js
@@ -6,6 +6,7 @@ import BoardAPIUtils from '../utils/BoardAPIUtils';
 
 var ActionTypes = Constants.ActionTypes;
 var CHANGE_EVENT = 'change';
+var SELECT_EVENT = 'index';
 
 var _boards = {};
 var _boardsLoading = true;
@@ -16,8 +17,8 @@ class BoardStore extends EventEmitter {
     super();
   }
 
-  emitChange() {
-    this.emit(CHANGE_EVENT);
+  emitChange(eventName) {
+    this.emit(eventName);
   }
 
   addChangeListener(callback) {
@@ -26,6 +27,15 @@ class BoardStore extends EventEmitter {
 
   removeChangeListener(callback) {
     this.removeListener(CHANGE_EVENT, callback);
+  }
+
+  //ボード選択のみのイベントリスナ
+  addSelectListener(callback) {
+    this.on(SELECT_EVENT, callback);
+  }
+
+  removeSelectListener(callback) {
+    this.removeListener(SELECT_EVENT, callback);
   }
 
   getBoards() {
@@ -57,32 +67,36 @@ AppDispatcher.register((payload) => {
       });
       if(Object.keys(_boards).length != 0) {
         _currentBoard = _boards[(Object.keys(_boards))[0]];
+        _BoardStore.emitChange(SELECT_EVENT);
       }
-      _BoardStore.emitChange();
+      _BoardStore.emitChange(CHANGE_EVENT);
       break;
     case ActionTypes.BOARDS.CREATE.SUCCESS_RESPONSE:
       _boards[action.board.id] = action.board;
       if(Object.keys(_currentBoard).length == 0) {
         _currentBoard = action.board;
+        _BoardStore.emitChange(SELECT_EVENT);
       }
-      _BoardStore.emitChange();
+      _BoardStore.emitChange(CHANGE_EVENT);
       break;
     case ActionTypes.BOARDS.UPDATE.SUCCESS_RESPONSE:
       _boards[action.board.id] = action.board;
-      _BoardStore.emitChange();
+      _BoardStore.emitChange(CHANGE_EVENT);
       break;
     case ActionTypes.BOARDS.DELETE.SUCCESS_RESPONSE:
       delete _boards[action.id];
       if(Object.keys(_boards).length != 0) {
         _currentBoard = _boards[(Object.keys(_boards))[0]];
+        _BoardStore.emitChange(SELECT_EVENT);
       }else {
         _currentBoard = {};
       }
-      _BoardStore.emitChange();
+      _BoardStore.emitChange(CHANGE_EVENT);
       break;
     case ActionTypes.BOARDS.CHANGE_CURRENT:
       _currentBoard = _boards[action.boardId];
-      _BoardStore.emitChange();
+      _BoardStore.emitChange(SELECT_EVENT);
+      _BoardStore.emitChange(CHANGE_EVENT);
       break;
   }
 });
