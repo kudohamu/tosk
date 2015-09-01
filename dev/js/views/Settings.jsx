e29ed18ac7513868649b8156b397e27e3c48b92a
kudohamu <kudohamu91086@yahoo.co.jp>
2015-09-01 09:51:53 +0900

    created: Settings component

---
+++ dev/js/views/Settings.jsx
@@ -0,0 +1,24 @@
+import React from 'react/addons';
+import Radium from 'radium';
+import Vendor from 'react-vendor-prefix';
+
+var styles = Vendor.prefix({
+});
+
+class Settings extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+Settings.propTypes = {
+};
+
+export default Radium(Settings);
